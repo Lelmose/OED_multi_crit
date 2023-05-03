98988cbdfa0f140a01a701fe347a9f49ade82462
Bastien <bastien-mollet@orange.fr>
2023-05-03 11:02:15 +0200

    Tests on pre-commit
    Dummy python file to test pre-commit

--- tests.py
+++ tests.py
@@ -1,3 +1,11 @@
-def function(t):
-    e = 2
-    return e
+"""Random functions to test pre-commit"""
+
+
+def function(tri: int):
+    """Return function useless.
+    :param tri: int
+    :returns: result
+    :rtype: int
+    """
+    exp = 2 + tri
+    return exp
